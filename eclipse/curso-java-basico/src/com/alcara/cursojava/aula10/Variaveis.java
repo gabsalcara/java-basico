b562c6dbd0a0f9919988f8ad49b26d1d095ec7aa
Gabriel Alcara <gabriel.alcara@terra.com.br>
2021-04-03 15:30:45 -0300

    Adicionando projeto das aulas no eclipse

---
+++ eclipse/curso-java-basico/src/com/alcara/cursojava/aula10/Variaveis.java
@@ -0,0 +1,19 @@
+package com.alcara.cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		// Convenção Java
+		int idade = 20;
+		String nome = "Gabriel";
+		String nomeDoMeuCachorro = "Totó";
+		String ano2014;
+		
+		idade = 25;
+		
+		System.out.println("Idade = " + idade);
+		System.out.println("Nome = " + nome);
+	}
+
+}
